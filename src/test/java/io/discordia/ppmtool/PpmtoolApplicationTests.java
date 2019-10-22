77cb90d9f0629e764a4f75f4f9d805ff056ad61a
dannyseymour <daniel@discordia.io>
2019-10-21 22:03:05 -0600

    first commit

---
+++ src/test/java/io/discordia/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.discordia.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
